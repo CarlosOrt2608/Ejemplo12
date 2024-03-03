979363f976ad9aeac2a7d7420da08f2d92698d12
CarlosOrt2608 <al071252@uacam.mx>
2024-03-02 21:57:52 -0600

    commit

--- x.java
+++ x.java
@@ -2,5 +2,6 @@ public class x {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("estrada");
+        System.out.println();
     }
 }
